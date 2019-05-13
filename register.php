fb8dfe3ac1f6c22b2f2b686a01cfc7175034aa6a
liza maiyuni <liza.maiyuni123@gmail.com>
2019-05-13 13:51:45 +0700

    menghubungkan halaman register ke database

--- register.php
+++ register.php
@@ -3,52 +3,58 @@ $servername = "localhost";
 $database = "stevedore job";
 $username = "root";
 $password = "";
-$email    = "email";
-$alamat   = "alamat";
-$umur     = "umur";
-$jeniskelamin = "jeniskelamin";
-$konfirmasipassword = "";
-
 
 // untuk tulisan bercetak tebal silakan sesuaikan dengan detail database Anda
 // membuat koneksi
-$conn = mysqli_connect($servername, $database, $username,  $password, $email, $alamat, $umur, $jeniskelamin, $konfirmasipassword, );
+$conn = mysqli_connect($servername, $username, $password, $database);
 // mengecek koneksi
 if (!$conn) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 echo "Koneksi berhasil";
-mysqli_close($conn);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Nelayan Part Time Job</title>
+	<title>Nelayan Partner Job</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-
+ergdsfgds
 	<h1>NELAYAN<br/>Partner Job</h1>
 
 	<div class="kotak_login">
 		<p class="tulisan_login">Silahkan Registrasi</p>
 
-		<form>
+		<form action="" method="post">
 			<label>Nama/Username</label>
 			<input type="text" name="nama" class="form_login" placeholder="Nama/username...">
 
 			<label>Email</label>
 			<input type="text" name="email" class="form_login" placeholder="Email...">
 
-			
+			<label>No HP</label>
+			<input type="int" name="no_hp" class="form_login" placeholder="No HP...">
+
+
+			<label>Alamat</label>
+			<input type="text" name="alamat" class="form_login" placeholder="Alamat...">
+
+			<label>Umur</label>
+			<input type="int" name="umur" class="form_login" placeholder="Umur...">
+
+			<label>Jenis Kelamin</label>
+			<input type="text" name="jk" class="form_login" placeholder="Jenis Kelamin...">
 
 			<label>Buat Password</label>
 			<input type="text" name="password" class="form_login" placeholder="Buat Password...">
+			<label><input id="checkbox" type="checkbox"> Tampilkan password</label>
 
 			<label>Konfirmasi Password</label>
 			<input type="text" name="konfirmasipassword" class="form_login" placeholder="Konfirmasi Password...">
+			<label><input id="checkbox" type="checkbox"> Tampilkan password</label>
 
-			<input type="submit" class="tombol_login" value="DAFTAR">
+			<input type="submit" class="tombol_login" name="submit" value="DAFTAR">
 
 			<br/>
 			<br/>
@@ -56,6 +62,18 @@ mysqli_close($conn);
 				<a class="link" href="#"></a>
 			</center>
 		</form>
+
+		<?php 
+			if (isset($_POST['submit'])) {
+				
+				$conn->query("INSERT INTO tabel_login VALUES('$_POST[nama]', '$_POST[email]', '$_POST[no_hp]', '$_POST[alamat]', '$_POST[umur]', '$_POST[jk]', '$_POST[password]', '$_POST[konfirmasipassword]' )");	
+
+			echo "<div class='btn btn-success'>";
+			echo "Data disimpan";
+			echo "</div>";
+			}
+
+		?>
 		
 	</div>
 
