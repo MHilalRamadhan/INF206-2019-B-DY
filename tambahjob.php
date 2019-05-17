6974f252b25a7054e4579186b1124682e7998c76
liza maiyuni <liza.maiyuni123@gmail.com>
2019-05-17 11:33:29 +0700

    menghubungkan ke database sehingga apa yang diinput tersimpan

--- tambahjob.php
+++ tambahjob.php
@@ -1,14 +1,36 @@
-<!DOCTYPE html>
+<?php
+$servername = "localhost";
+$database = "stevedore job";
+$username = "root";
+$password = "";
 
+// untuk tulisan bercetak tebal silakan sesuaikan dengan detail database Anda
+// membuat koneksi
+$conn = mysqli_connect($servername, $username, $password, $database);
+// mengecek koneksi
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+echo "Koneksi berhasil";
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
 <link rel="stylesheet" href="styletj.css">
 
 <div class="container">
-            <form class="form-horizontal" role="form">
+            <form class="form-horizontal" role="form" action="" method="post">
                 <h2>Tambah Job</h2>
                 <div class="form-group">
-                    <label for="NamaPelabuhan" class="col-sm-3 control-label">Nama Pelabuhan</label>
+
+                    <label>Username Nelayan</label>
                     <br>
-                    
+                    <input type="text" name="username" class="form_login" placeholder="Username Nelayan...">
+                    <br>
+                    <br>
+                    <label for="NamaPelabuhan" class="form_login">Nama Pelabuhan</label>
+                    <br>
                     <select name="daftarpelabuhan" id="daftarpelabuhan">
                         <option>Pilih Daftar Pelabuhan Perikanan</option>
                         <option value="lampulo">Lampulo</option>
@@ -16,13 +38,13 @@
                         <option value="aluenaga">Alue naga</option>
                         <option value="gampongjawa">Gampong jawa</option>
                     </select>
-                    
+                    <br>
                     <br>
                 </div>
                 
                 
                 <div class="form-group">
-                    <label for="JlhPekerja" class="col-sm-3 control-label">Jumlah Pekerja Yang Dibutuhkan</label>
+                    <label for="JlhPekerja" class="form_login">Jumlah Pekerja Yang Dibutuhkan</label>
                     <br>
                     
                     <select name="jumlah" id="jumlah">
@@ -54,76 +76,68 @@
                         <option value="25">25</option>
                         
                     </select>
-                    
+                    <br>
                     <br>
                 </div>
                 <div class="form-group">
-                    <label for="jnspekerjaan" class="col-sm-3 control-label">Jenis Pekerjaan</label>
+                    <label for="jnspekerjaan" class="form_login">Jenis Pekerjaan</label>
                     <br>
                     
-                    <select name="jenis" id="jumlah">
+                    <select name="jenis" id="jenis">
                         <option>Jenis Pekerjaan</option>
                         <option value="Mengangkutikan">Mengangkut Ikan</option>
                         <option value="Memilahjenisikan">Memilah Jenis Ikan</option>
                         <option value="Membersihkan">Membersihkan</option>
                         <option value="Lainnya">Lainnya</option>
                     </select>
-
-                <div class="form-group">
-                        <label for="Height" class="col-sm-3 control-label">Keterangan jenis lainnya</label>
-                    <div class="col-sm-9">
-                        <input type="Keterangan" id="height" placeholder="Keterangan" class="form-control">
-                    </div>
                 </div>
-
-                </div>
+                <br>
+                <br>
                 <div class="form-group">
-                    <label for="Gaji" class="col-sm-3 control-label">Gaji Per Jam</label>
+                        <label for="Height" class="form_login">Keterangan jenis lainnya</label>
                     <div class="col-sm-9">
-                        <input type="Gaji" id="Gaji" placeholder="Rp.xxxxxxx" class="form-control">
-                    </div>
-                </div>
-                <div class="form-group">
-                    <label for="phoneNumber" class="col-sm-3 control-label">Kontak </label>
-                    <div class="col-sm-9">
-                        <input type="phoneNumber" id="phoneNumber" placeholder="Phone number" class="form-control">
-                    </div>
-                </div>
-                <div class="form-group">
-                        <label for="Height" class="col-sm-3 control-label">Keterangan Nelayan</label>
-                    <div class="col-sm-9">
-                        <input type="Keterangan" id="height" placeholder="Keterangan" class="form-control">
+                        <input type="text" name="keterangan" class="tambah_job" placeholder="Selain pilihan yang diatas">
                     </div>
                 </div>
 
+                </div>
+                <br>
                 <div class="form-group">
-                    <label class="control-label col-sm-3">Jenis Pekerjaan</label>
-                    <div class="col-sm-6">
-                        <div class="row">
-                            <div class="col-sm-4">
-                                <label class="radio-inline">
-                                    <input type="radio" id="femaleRadio" value="Female">Angkut ikan
-                                </label>
-                            </div>
-                            <div class="col-sm-4">
-                                <label class="radio-inline">
-                                    <input type="radio" id="maleRadio" value="Male">Hitung Ikan
-                                </label>
-                            </div>
-                            <div class="col-sm-4">
-                                <label class="radio-inline">
-                                    <input type="radio" id="femaleRadio" value="Female">Pilah Ikan
-                                </label>
-                            </div>
-                            <div class="col-sm-4">
-                                <label class="radio-inline">
-                                    <input type="radio" id="femaleRadio" value="Female">Bersihkan Ikan
-                                </label>
-                            </div>
-                        </div>
+                    <label for="Gaji" class="form_login">Upah Per Jam</label>
+                    <div class="form_login" >
+                        <input type="text" name="gaji" class="tambah_job" placeholder="Jumlah Upah per Jam..">
                     </div>
-                </div> <!-- /.form-group -->
+                </div>
+                <br>
+                <div class="form-group">
+                    <label for="phoneNumber" class="form_login">Kontak </label>
+                    <div class="col-sm-9">
+                        <input type="text" name="phoneNumber" class="tambah_job" placeholder="No HP..">
+                    </div>
+                </div>
+                <br>
+                <div class="form-group">
+                        <label for="Height" class="form_login">Keterangan Nelayan</label>
+                    <div class="col-sm-9">
+                        <input type="text" name="keterangannelayan" class="tambah_job" placeholder="Keterangan lain untuk Nelayan..">
+                    </div>
+                </div>
+                <br>
+                <!-- /.form-group -->
 
-                <button type="submit" class="btn btn-primary btn-block">SUBMIT</button>
+                <input type="submit" class="tombol_login" name="submit" value="TAMBAH">
             </form> <!-- /form -->
+            <?php 
+            if (isset($_POST['submit'])) {
+                
+                $conn->query("INSERT INTO tambahjob(username, daftarpelabuhan, jumlah, jenis, keterangan, gaji, phoneNumber, Keterangannelayan) VALUES('$_POST[username]', '$_POST[daftarpelabuhan]', '$_POST[jumlah]', '$_POST[jenis]', '$_POST[keterangan]', '$_POST[gaji]', '$_POST[phoneNumber]', '$_POST[keterangannelayan]')");    
+
+            echo "<div class='btn btn-success'>";
+            echo "Data disimpan";
+            echo "</div>";
+            }
+
+            ?>
         </div> <!-- ./container -->
+    </body>
+</html>
